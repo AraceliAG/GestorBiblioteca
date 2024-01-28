e7c67b1a11b27cb13182460aeadadac734973db7
AraceliAG <131417012+AraceliAG@users.noreply.github.com>
2024-01-28 15:45:23 -0600

    Conexion BD
    Conexion BD

---
+++ config/conexion.js
@@ -0,0 +1,21 @@
+var mysql = require("mysql");
+
+var con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'biblioteca'
+});
+
+con.connect (
+    (err) => {
+        if (!err){
+            console.log("CONEXION EXITOSA :D ")
+
+        } else{
+            console.log("NO SE LOGRÓ ESTABLECER CONEXION")
+        }
+    }
+);
+
+module.exports = con;
--- controller/librosController.js
+++ controller/librosController.js
@@ -1,7 +1,10 @@
 /*SE AGREGA INFORMACIÓN QUE PUEDE MANIPULAR TODOS LOS DATOS QUE SE ENCUENTRAN EN ROUTES*/
-
+var con = require("../config/conexion");
 module.exports ={
     index:function(req, res){    /*SE CREA FUNCION*/
+    con.query("SELECT * FROM libros", function(err, datos){
+        console.log(datos);
+    });
     res.render('libros/index', {title:'Aplication'});  /*SE VA PRIMERO A LA RUTA VISTA/LIBROS/INDEX)*/
 
     }
