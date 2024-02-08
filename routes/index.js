2442e082cc650805bd3214782d829c34686e4001
AraceliAG <131417012+AraceliAG@users.noreply.github.com>
2024-02-07 20:59:56 -0600

    Implementación Bootstrap
    Implementación Bootstrap herramienta la cual aytudará al diseño de la pagina

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-
+/*RUTA PRINCIPALL*/
 /* GET home page. */
 router.get('/', function(req, res, next){
     res.send("BIENVENIDO")
