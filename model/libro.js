f12078484b476a1177085b9dfefe7d19339a22b2
AraceliAG <131417012+AraceliAG@users.noreply.github.com>
2024-02-14 21:54:08 -0600

    FUNCION INSERTAR
    Se crea función insertar en nuestro modelo, en está función se piden 3 parámetros, conexión, datos los cuales son los que se envian a partir del req.body y luego se cre la sentencia con el parametro conexion y consulta. Los corchetes toman el valor name que encontramos en nuestro archivo de la vista, y por ultimo se queda la funcion el cual ejecuta si existe un error.
    En nuestro controlador , se modifica nuestra función guardar en este caso primero imprimimos la información que se manda al formullario y se muestra en consola, en seguida con nuestra variabkes libros llama a lo que es nuestra funcion insertar y se pasan los siguiente parameros y despues se redirecciona todo en nuestra vista libros para mostrar el archivo.

--- model/libro.js
+++ model/libro.js
@@ -4,5 +4,12 @@ INTERACTUAR CON LOS REGISTROS*/
 module.exports = {
     obtener:function(conexion, funcion){
         conexion.query("SELECT * FROM libros", funcion);
+    },
+
+    /*SE CREA NUEVA FUNCION PARA INSERTAR DATOS */
+
+    insertar:function(conexion, datos, funcion){ /*SE AGREGA EL PARAMETRO DATOS YA QUE ESTARA ENVIANDO LOS QUE SON LOS DATOS req.body */
+        /*AQUI EN LOS CORCHETES SON LOS DATOS QUE ESTEN DENTRO DEL VIEW en el name DE CA ATRIBUTO QUE SE PIDE */
+        conexion.query("INSERT INTO libros (nombre, imagen) VALUES (?,?)", [datos.nombre, datos.archivo], funcion);
     }
 }
